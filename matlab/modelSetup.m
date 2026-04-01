d65f8b811cabdb700440e1b40c5eaa8258154b3a
cornflow <marko.corn@gmail.com>
2026-04-01 10:23:51 +0200

    Add cache files for modelGA simulation
    - Created checksumOfCache.mat to store cache checksums for modelGA.
    - Added simulink_cache.xml for file attributes related to the modelGA cache.
    - Introduced varInfo.mat to hold variable information for modelGA simulation.

--- matlab/modelSetup.m
+++ matlab/modelSetup.m
@@ -1,24 +1,24 @@
 % FlexDrive mechanical model - torque input
 % Input:  u = M (torque) [N*m]
-% Outputs: y = [phi1; phi2]
+% Outputs: y = [phi1 (deg); phi2 (deg); omega1 (rpm); omega2 (rpm)]
 % States:  x = [phi1; dphi1; phi2; dphi2]
 
 clear; clc;
 
 %% PARAMETERS
 
-% Inertias (converted from g*m^2 to kg*m^2)
-J1 = 0.612e-3;
-J2 = 0.0902e-3;
-J3 = 1.0e-6;      % small nonzero value (important!)
+% Inertias [kg*m^2] -- identified by GA
+J1 = 1.7830e-4;
+J2 = 7.6620e-5;
+J3 = 4.2527e-6;
 
 JL = J1 + J2;
 JD = J3;
 
-% Mechanical parameters (you will tune/identify these)
-k  = 0.05;        % [N*m/rad]
-B1 = 1e-4;        % [N*m*s/rad]
-B2 = 1e-4;        % [N*m*s/rad]
+% Mechanical parameters -- identified by GA (ISE = 5.83e7)
+k  = 9.6660;      % [N*m/rad]    spring stiffness
+B1 = 1.9318e-4;   % [N*m*s/rad]  load-side damping
+B2 = 1.0787e-4;   % [N*m*s/rad]  drive-side damping
 
 %% STATE-SPACE MATRICES
 
@@ -32,10 +32,11 @@ B = [0;
      0;
      0];
 
-C = [1 0 0 0;    % phi1
-     0 0 1 0];   % phi2
+C = [180/pi  0       0        0;       % phi1   [deg]
+     0       0       180/pi   0;       % phi2   [deg]
+     0       30/pi   0        0;       % omega1 [rpm]
+     0       0       0        30/pi];  % omega2 [rpm]
 
-D = [0;
-     0];
+D = [0; 0; 0; 0];
 
-Kpwm_M=1.925e-5;
+pwm = 2000;
